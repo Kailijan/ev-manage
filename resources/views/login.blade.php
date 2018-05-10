60c28268e2a899ad5dd6e4875af9793db4fde64c
kailijan <kai.frankenhaeuser@t-online.de>
2018-05-10 17:07:41 +0200

    Setup Laravel

---
+++ resources/views/login.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.master")
+
+@section("title")
+    Login
+@stop
+
+@section("content")
+    @include("elements.nav")
+    <div class="page-login">
+        Login
+    </div>
+@stop
